ab504e24e825ea427a47a47caac8da46a694fa72
ekignatenko <egorignatenko53@gmail.com>
2026-06-26 10:39:11 +0300

    Арифметические операции и вывод в консоль

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 777;
+        int b = 222;
+        System.out.println("Сумма:"+ (a + b));
+        System.out.println("Умножение:"+ (a * b));
+        System.out.println("Разность:"+ (a - b));
+        System.out.println("Остаток:"+ (a % b));
+        System.out.println("деление:"+ (a / b));
+    }
+}
